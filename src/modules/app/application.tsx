4827cd7debff216b6ab9ab393f81efac22cd4079
benskj <benjaminskj@hotmail.com>
2025-01-28 15:32:30 +0100

    added css, dropbox for stadia or openstreetmap and functioning stadia watercolor map

--- src/modules/app/application.tsx
+++ src/modules/app/application.tsx
@@ -1,11 +1,13 @@
-import React, { useEffect, useRef } from "react";
+import React, { ChangeEvent, useEffect, useRef, useState } from "react";
 import { Map, View } from "ol";
 import TileLayer from "ol/layer/Tile";
-import { OSM } from "ol/source";
+import { OSM, StadiaMaps } from "ol/source";
 import { useGeographic } from "ol/proj";
 
 // Styling of OpenLayers components like zoom and pan controls
 import "ol/ol.css";
+
+import "./application.css";
 
 // By calling the "useGeographic" function in OpenLayers, we tell that we want coordinates to be in degrees
 //  instead of meters, which is the default. Without this `center: [10.6, 59.9]` brings us to "null island"
@@ -15,22 +17,43 @@ useGeographic();
 //  map data structure will be used
 const map = new Map({
   // The map will be centered on a position in longitude (x-coordinate, east) and latitude (y-coordinate, north),
-  //   with a certain zoom level
+
   view: new View({ center: [10.8, 59.9], zoom: 13 }),
-  // map tile images will be from the Open Street Map (OSM) tile layer
-  layers: [new TileLayer({ source: new OSM() })],
 });
 
 // A functional React component
+const osmLayer = new TileLayer({ source: new OSM() });
+
+const stadiaLayer = new TileLayer({
+  source: new StadiaMaps({ layer: "stamen_watercolor" }),
+});
+
 export function Application() {
-  // `useRef` bridges the gap between JavaScript functions that expect DOM objects and React components
+  const [layers, setLayers] = useState([osmLayer]);
+  useEffect(() => {
+    map.setLayers(layers);
+  }, [layers]);
   const mapRef = useRef<HTMLDivElement | null>(null);
-  // When we display the page, we want the OpenLayers map object to target the DOM object refererred to by the
-  // map React component
+
   useEffect(() => {
     map.setTarget(mapRef.current!);
   }, []);
 
+  function handleChange(e: ChangeEvent<HTMLSelectElement>) {
+    if (e.target.value === "osm") setLayers([osmLayer]);
+    else if (e.target.value === "stadia") setLayers([stadiaLayer]);
+  }
+
   // This is the location (in React) where we want the map to be displayed
-  return <div ref={mapRef}></div>;
+  return (
+    <>
+      <nav>
+        <select onChange={handleChange}>
+          <option value="osm">Open Street Map</option>
+          <option value="stadia">Stadia</option>
+        </select>
+      </nav>
+      <div ref={mapRef}></div>
+    </>
+  );
 }
